c8160fb2c75d2adfb11d1cddb0c9413045eaad44
astra <26215963+3tternp@users.noreply.github.com>
2024-04-05 11:40:02 +0545

    Add files via upload

---
+++ powerpipe.sh
@@ -0,0 +1,5 @@
+sudo mkdir powerpipe_dashboards
+sudo cd powerpipe_dashboards
+sudo powerpipe mod init
+sudo powerpipe mod install https://github.com/turbot/steampipe-mod-aws-insights.git
+sudo powerpipe server
